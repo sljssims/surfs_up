7977c9aca3152d6425d923cb4740270e26482761
sljssims <sljssims@gmail.com>
2021-10-13 00:03:15 -0500

    cleaned up flask code

--- Challenge/app.py
+++ Challenge/app.py
@@ -43,12 +43,12 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
+    Welcome to the Climate Analysis API!</br>
+    Available Routes:</br>
+    /api/v1.0/precipitation</br>
+    /api/v1.0/stations</br>
+    /api/v1.0/tobs</br>
+    /api/v1.0/temp/start/end</br>
     ''')
 
 
@@ -59,18 +59,26 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    precipitation = session.query(measurement.date, measurement.prcp).\
-     filter(measurement.date >= prev_year).all()
+    precipitation = session.query(measurement.date, measurement.prcp).filter(measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
+     
+     # Close Session
+    session.close()
+
     return jsonify(precip)
+    
 
 ##########################
 # Stations Route
 ##########################
 @app.route("/api/v1.0/stations")
+
 def stations():
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
+    # Close Session
+    session.close()
+    
     return jsonify(stations=stations)
 
 ##########################
@@ -79,10 +87,13 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    results = session.query(measurement.tobs).\
-        filter(measurement.station == 'USC00519281').\
-        filter(measurement.date >= prev_year).all()
-    temps = list(np.ravel(results))    
+
+    results = session.query(measurement.tobs).filter(measurement.station == 'USC00519281').filter(measurement.date >= prev_year).all()
+    
+    temps = list(np.ravel(results))
+     # Close Session
+    session.close()    
+    
     return jsonify(temps=temps)
 
 ###############################
@@ -91,19 +102,21 @@ def temp_monthly():
 #Start and End Date
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
+
 def stats(start=None, end=None):
     sel = [func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
+
     if not end:
-        results = session.query(*sel).\
-            filter(measurement.date >= start).all()
+        results = session.query(*sel).filter(measurement.date >= start).all()
         temps = list(np.ravel(results))
-        return jsonify(temps=temps)
+        return jsonify(temps)
     
-    results = session.query(*sel).\
-        filter(measurement.date >= start).\
-        filter(measurement.date <= end).all()
+    results = session.query(*sel).filter(measurement.date >= start).filter(measurement.date <= end).all()
     temps = list(np.ravel(results))
-    return jsonify(temps=temps)
+    
+     # Close Session
+    session.close()
+    return jsonify(temps)
 
 ############################
 # Define main behavior
